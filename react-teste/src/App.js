d50bbc4eb82f61bbcce80288cc73e9d5a9df34bf
Lais <laiscristina.animamundi@gmail.com>
2025-01-12 15:40:26 -0300

    lllll

--- react-teste/src/App.js
+++ react-teste/src/App.js
@@ -10,9 +10,9 @@ const App = () => {
   const fetchCatImage = async () => {
     setLoading(true); // Define loading como verdadeiro ao iniciar a requisição
     try {
-      // Substitua a URL abaixo pela URL do seu backend na Vercel
-      const response = await axios.get('https://testando-ewbe.vercel.app/'); // Ajuste a URL aqui
-      setCatUrl(response.data[0].url); // Ajuste para acessar o URL correto da resposta
+      // URL do seu backend na Vercel
+      const response = await axios.get('https://testando-ewbe.vercel.app/gatinhos'); // Ajuste a URL aqui
+      setCatUrl(response.data[0].url); // Acesse a URL da imagem corretamente
       setLoading(false); // Define loading como falso após a resposta
     } catch (err) {
       setError('Não foi possível obter a imagem do gatinho.');
